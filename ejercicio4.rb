fa7a7997270e39b595db40497b93ddfa91e79464
pbodr <pbolivares@gmail.com>
2018-05-27 15:13:29 -0400

    ejercicio5

--- ejercicio4.rb
+++ ejercicio4.rb
@@ -1,5 +1,6 @@
-Transformar la clase Semana en un módulo y obtener la misma salida:
-class Semana
+#Transformar la clase Semana en un módulo y obtener la misma salida:
+
+module Semana
  @@primer_dia = 'Lunes'
  def self.primer_dia
  @@primer_dia
@@ -14,3 +15,24 @@ end
 puts "La semana comienza el día #{Semana.primer_dia}"
 puts Semana.en_un_meses
 puts Semana.en_un_año
+
+#
+# class Semana
+#  @@primer_dia = 'Lunes'
+#  def self.primer_dia
+#  @@primer_dia
+#  end
+#  def self.en_un_meses
+#  "Un mes tiene 4 semanas."
+#  end
+#  def self.en_un_año
+#  "Un año tiene 52 semanas."
+#  end
+# end
+# puts "La semana comienza el día #{Semana.primer_dia}"
+# puts Semana.en_un_meses
+# puts Semana.en_un_año
+
+# La semana comienza el día Lunes
+# Un mes tiene 4 semanas.
+# Un año tiene 52 semanas.
